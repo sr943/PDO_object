ffe2bb931471c9ac88da5a8f198ebae3a453ede0
shubhangi rakhe <sr943@afsconnect2.njit.edu>
2017-11-03 00:57:34 -0400

    count

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
  
 class main {
   public function __construct(){
-
+ 
 
 $servername = "sql1.njit.edu";
 $username = "sr943";
@@ -16,42 +16,72 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=sr943", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<h3>Connected successfully </h3><br>";  //conection message
-    $sel = new select();
-    $sel->fetch($conn);
+    echo "<h3>Connected successfully </h3><br>";    //conection message
+    echo "<hr>";
+    $sel = new select();   //creating an object
+    $sel->fetch($conn);    // calling function
+   
+
 
      }
 catch(PDOException $e)
     {
     echo "<h3>Connection failed:</h3> <br>" . $e->getMessage();
     }
-  }
+ 
 }
+
+}
+
+
+
+
+
 class select{
   public function fetch($conn){
 
-    $stmt = $conn->prepare("SELECT * FROM accounts where id < 6"); 
+    $stmt = $conn->prepare("SELECT * FROM accounts where id < 6");    //fetching data from DB
     $stmt->execute();
 
 
-      $result = $stmt->fetchAll(PDO::FETCH_ASSOC); 
-      echo "<table style='border: 1px solid black'>";
+
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);       // using associative array  
+      $co = new count();
+      $co->counting($result);
+
+
+
+echo  "<table style='border: 1px solid black'>";
       foreach ($result as $row) {
     
-      echo "<tr>";
+      echo  "<tr>";
 
       foreach ($row as $column) {
       echo "<td style='border: 1px solid black'>$column </td>";
       
    }
-   echo "</tr>";
+  echo  "</tr>";
 }    
-echo "</table>";
+echo  "</table>";
+
     
-   
+  
 
 $conn = null;
 }
+}
+
+class count{
+  public function counting($result){
+
+
+echo "<h3>Number of rows are:</h3>";
+print_r(count($result));
+echo"<hr>";
+
+  }
+
+
 }
 
 
